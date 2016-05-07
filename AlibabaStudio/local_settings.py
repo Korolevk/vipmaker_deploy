5ad293e8c3a7dd3532e8301040dfe0116d905cc2
Kiriill <op.96@mail.ru>
2016-05-08 00:34:33 +0700

    Bugs fixed

--- AlibabaStudio/local_settings.py
+++ AlibabaStudio/local_settings.py
@@ -3,6 +3,7 @@
 SECRET_KEY = '0mgys*rp#qgel!@--h4tqxg52lr)9l5n=c7y3j3f)hv*b@rmc#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# False in production
 DEBUG = True
 
 # Database
